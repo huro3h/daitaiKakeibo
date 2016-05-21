6e87905badc15001807a1c476fe31890c7ca619d
huro3h_satoshi <huro3h@gmail.com>
2016-05-21 10:37:02 +0800

    設定画面作成開始

---
+++ daitaiKakeibo/setteiController.swift
@@ -0,0 +1,31 @@
+//  setteiController.swift
+//  daitaiKakeibo
+//  Created by satoshiii on 2016/05/21.
+//  Copyright © 2016年 satoshiii. All rights reserved.
+
+import UIKit
+
+class setteiController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+	}
+
+
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//    }
+	
+
+}

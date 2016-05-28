67ffc3b1e0bf14d69c0c7eccd15f9ca5e78d4b75
huro3h_satoshi <huro3h@gmail.com>
2016-05-28 21:10:59 +0800

    期間選択削除 日付の値をString型で取得するところまで

--- daitaiKakeibo/eraseController.swift
+++ daitaiKakeibo/eraseController.swift
@@ -9,31 +9,46 @@ import CoreData
 class eraseController: UIViewController {
 
 	var dataArray: [String] = []
+	var catchFromDate: String = ""
+	var catchUntilDate: String = ""
+	let now = NSDate()
+	let df = NSDateFormatter()
+	
+	@IBOutlet weak var fromDatePicker: UIDatePicker!
+	@IBOutlet weak var untilDatePicker: UIDatePicker!
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
 		read()
-
+		
+		// DatePicker設定
+		fromDatePicker.datePickerMode = UIDatePickerMode.Date
+		untilDatePicker.datePickerMode = UIDatePickerMode.Date
+		
+		df.dateFormat = "yyyy/MM/dd"
+		fromDatePicker.date = df.dateFromString("2016/04/01")!
+		fromDatePicker.minimumDate = df.dateFromString("2000/01/01")
+		fromDatePicker.maximumDate = df.dateFromString("2045/12/31")
+		untilDatePicker.date = df.dateFromString("2016/04/01")!
+		untilDatePicker.minimumDate = df.dateFromString("2000/01/01")
+		untilDatePicker.maximumDate = df.dateFromString("2045/12/31")
+		
+		// 今日の日付取得
+		fromDatePicker.date = now
+		untilDatePicker.date = now
 	}
 	
-	@IBOutlet weak var fromPicker: UIPickerView!
-	@IBOutlet weak var untilPicker: UIPickerView!
-	
-	func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-		return 1
+	// 期間選択
+	@IBAction func changeFromPicker(sender: UIDatePicker) {
+		let selectedFromDate: NSString = df.stringFromDate(sender.date)
+		catchFromDate = selectedFromDate as String
+		print(catchFromDate)
 	}
 	
-	func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-		return dataArray.count
-	}
-	
-	func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-		return dataArray[row]
-	}
-	
-	func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-		let i = row+1
-		print("選択されたのは\(i)行目の\(dataArray[row])です")
+	@IBAction func changeUntilPicker(sender: UIDatePicker) {
+		let selectedUntilDate: NSString = df.stringFromDate(sender.date)
+		catchUntilDate = selectedUntilDate as String
+		print(catchUntilDate)
 	}
 	
 	
@@ -47,7 +62,7 @@ class eraseController: UIViewController {
 		alertController.addAction(UIAlertAction(
 			title: "消去",
 			style: UIAlertActionStyle.Destructive,
-			handler: { action in self.deleteData()}))
+			handler: { action in self.deletePartData()}))
 		
 		alertController.addAction(UIAlertAction(
 			title: "Cancel",
@@ -63,7 +78,7 @@ class eraseController: UIViewController {
 		alertController.addAction(UIAlertAction(
 			title: "消去",
 			style: UIAlertActionStyle.Destructive,
-			handler: { action in self.deleteData()}))
+			handler: { action in self.deleteAllData()}))
 		
 		alertController.addAction(UIAlertAction(
 			title: "Cancel",
@@ -91,14 +106,14 @@ class eraseController: UIViewController {
 					print("日時:\(accountBook.inputDate), 食費:\(accountBook.foodFee)")
 					
 					// 用意した変数に各項目を配列の形で代入
-					let fixDate = dateString(accountBook.inputDate!)
-					let myFoods = String(accountBook.foodFee!)
-					let myLifes = String(accountBook.lifeFee!)
-					let myZappies = String(accountBook.zappiFee!)
-					let myHokas = String(accountBook.hokaFee!)
-					let myTotals = String(accountBook.totalFee!)
+					//let fixDate = dateString(accountBook.inputDate!)
+					//let myFoods = String(accountBook.foodFee!)
+					//let myLifes = String(accountBook.lifeFee!)
+					//let myZappies = String(accountBook.zappiFee!)
+					//let myHokas = String(accountBook.hokaFee!)
+					//let myTotals = String(accountBook.totalFee!)
 					
-					dataArray.append("\(fixDate)\(myFoods)\(myLifes)\(myZappies)\(myHokas)\(myTotals)")
+					//dataArray.append("\(fixDate)\(myFoods)\(myLifes)\(myZappies)\(myHokas)\(myTotals)")
 					
 				}
 			} catch let error1 as NSError {
@@ -107,8 +122,40 @@ class eraseController: UIViewController {
 		}
 	}
 	
-	func deleteData (){
-		// CoreData削除
+	func deletePartData (){
+		// CoreData期間削除
+		let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+		if let managedObjectContext: NSManagedObjectContext = appDelegate.managedObjectContext {
+			let entityDiscription = NSEntityDescription.entityForName("AccountBook", inManagedObjectContext: managedObjectContext)
+			
+			let fetchRequest = NSFetchRequest(entityName: "AccountBook")
+			fetchRequest.entity = entityDiscription
+			
+			// 以下の2行で期間指定
+			let predicate = NSPredicate(format: "%K = %@", "title", "hogehoge")
+			fetchRequest.predicate = predicate
+			
+			var error: NSError? = nil
+			
+			do {
+				let results = try managedObjectContext.executeFetchRequest(fetchRequest)
+				print(results.count)
+				
+				for managedObject in results{
+					let accountBook = managedObject as! AccountBook
+					
+					managedObjectContext.deleteObject(managedObject as! NSManagedObject)
+					
+					appDelegate.saveContext()
+				}
+			} catch let error1 as NSError {
+				error = error1
+			}
+		}
+	}
+	
+	func deleteAllData (){
+		// CoreData全削除
 		let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
 		if let managedObjectContext: NSManagedObjectContext = appDelegate.managedObjectContext {
 			let entityDiscription = NSEntityDescription.entityForName("AccountBook", inManagedObjectContext: managedObjectContext)

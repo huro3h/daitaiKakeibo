6b5e41e6fb53c5e3e06b4fd7b4ae6b93f2b5b95d
huro3h_satoshi <huro3h@gmail.com>
2016-05-29 17:52:06 +0800

    変換する前のdate型を取り出すも、どうやって条件指定するか思いつかず別の方法を考える( - "-;)

--- daitaiKakeibo/eraseController.swift
+++ daitaiKakeibo/eraseController.swift
@@ -123,8 +123,29 @@ class eraseController: UIViewController {
 		}
 	}
 	
+	// CoreDataから日付を検索するやり方 ?
+//	CoreDataのNSPredicateで日付を検索する方法を教えて下さい
+//	
+//	例えばCoreDataで"Data"というエンティティを作成したとして、
+//	その中にType：DateのAttribute、"day"を作成したとします。
+//	そして特定の日付(時間は無視します)のdayを引っ張ろうと試みた時に、次のようなコードとなりました。
+
+//	let predicate = NSPredicate(format: "%K = ?????", "day", ?????)
+
+//	 let calendar :NSCalendar! = NSCalendar(identifier: NSCalendarIdentifierGregorian)
+//	 let targetedDay :NSDate! = calendar.dateWithEra(1, year: 2015, month: 5, day: 12, hour: 0, minute: 0, second: 0, nanosecond: 0)
+//	 let predicate = NSPredicate(format: "SELF.day BETWEEN {%@ , %@}", targetedDay, NSDate(timeInterval: 24*60*60-1, sinceDate: targetedDay))
+	
+//	例えば、特定のNSDateを作って、そこからの24時間-1秒をBETWEENで取るという手があります。
+//	dayのattributeには、SELF.dayを使います。
+//	ミリ秒までデータがあるならば、SELF.day >= %@ AND SELF.day < %@で厳密に期間判定をした方が良いです。
+	
 	func deletePartData (){
 	
+		// 目的の日時と比較させる日付作成(2016年5月1日から今日が何日目かをカウントさせる)
+		let calendar :NSCalendar! = NSCalendar(identifier: NSCalendarIdentifierGregorian)
+		let targetedDay :NSDate! = calendar.dateWithEra(1, year: 2016, month: 5, day: 29, hour: 0, minute: 0, second: 0, nanosecond: 0)
+		
 		// CoreData期間削除
 		let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
 		if let managedObjectContext: NSManagedObjectContext = appDelegate.managedObjectContext {
@@ -134,7 +155,9 @@ class eraseController: UIViewController {
 			fetchRequest.entity = entityDiscription
 			
 			// 以下の2行でデータ削除期間の指定
-			let predicate = NSPredicate(format: "inputDate CONTAINS %@", "2016-05-29")
+//			 let predicate = NSPredicate(format: "%K = ?????", "inputDate", "2016-05-29")
+			let predicate = NSPredicate(format: "SELF.day BETWEEN {%@ , %@}", targetedDay, NSDate(timeInterval: 24*60*60-1, sinceDate: targetedDay))
+			print(predicate)
 			fetchRequest.predicate = predicate
 			var error: NSError? = nil
 			
